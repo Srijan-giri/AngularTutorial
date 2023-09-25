277a18fad4d643fd34258b103c6d17000660817c
Srijan-giri <srijan_g.ece2020@msit.edu.in>
2023-09-25 13:55:59 +0530

    component Added

---
+++ first-project/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+// decorator
+@Component(
+  {
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  }
+)
+export class AppComponent {
+  // title = 'first-project';
+  title = 'we are learning about component';
+  // data & logic 
+}
